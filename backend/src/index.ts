372f41faf1b230055761cc0d4dd2928e7597e43d
m.podrezov <m.podrezov@etpgpb.ru>
2023-04-20 12:03:48 +0300

    Add base modules to express.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,12 @@
 import express from 'express'
+import cors from 'cors'
+import logger from 'morgan'
 
 const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(logger('dev'))
 
 const Port = process.env.Port || 8080
 
