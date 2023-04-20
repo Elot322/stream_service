a27381cb4670cd17717f3a4e1aff3db61a213c83
m.podrezov <m.podrezov@etpgpb.ru>
2023-04-20 14:43:42 +0300

    Downgrade WebTorrent and create instance.

--- backend/src/modules/stream/stream.controller.ts
+++ backend/src/modules/stream/stream.controller.ts
@@ -1,6 +1,12 @@
 import { Router, Request, Response } from 'express'
+import WebTorent from 'webtorrent'
 
 const router = Router()
+const client = new WebTorent()
+
+let state = {
+
+}
 
 router.get('/', (req: Request, res: Response) => {
   res.status(200).send({
