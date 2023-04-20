eae107cbfa366a7a7bafe51b86ae6085901549cd
m.podrezov <m.podrezov@etpgpb.ru>
2023-04-20 14:27:50 +0300

    Add router

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,15 @@ import express from 'express'
 import cors from 'cors'
 import logger from 'morgan'
 
+import streamRouter from './modules/stream/stream.controller'
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(logger('dev'))
+
+app.use('/stream', streamRouter)
 
 const Port = process.env.Port || 8080
 
